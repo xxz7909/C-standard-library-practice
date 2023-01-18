4ab05aa1ca742c8385714f0c5593dfb36e91d169
xixi <2990698929@qq.com>
2023-01-18 19:29:40 +0800

    update notes

--- float.h/float.c
+++ float.h/float.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <float.h>
 
+// tmd一堆宏 然后是缩写，有时间再研究什么意思
+// FLT_MAX FLT_MIN这种倒是挺实用
 int main(int argc, const char* argv[])
 {
-	printf("%d\n",FLT_DECIMAL_DIG);
-	printf("%d\n",FLT_ROUNDS);
+	printf("The radix of exponent:%d\n", FLT_RADIX);
+	printf("float point rounding mode:%d\n",FLT_ROUNDS);
 
 	return 0;
 }
